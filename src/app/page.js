12ff75e8f5b66b05348c24e7327acb3467988c55
Ibrahim khan <ibrahim2013khan@gmail.com>
2024-08-13 23:47:18 +0600

    Ecommerce-Project-With-Nextjs-and-TailwindCSS

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import NewProduct from "@/components/NewProduct";
 
 export default function Home() {
   return (
     <main>
        <Hero />
+       <NewProduct />
     </main>
   );
 }
